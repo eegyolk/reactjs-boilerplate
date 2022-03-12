27f0e4f29d937b08e0beb8b7943cd3e44ac73e13
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-12 14:53:26 +0800

    Removed some contents because it was transfered in Home page

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import App from './index.js';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />, { wrapper: MemoryRouter });
 });
