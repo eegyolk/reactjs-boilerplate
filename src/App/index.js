673b3ec32c915377e798bf3b72f1fcbf780f51b9
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-12 12:12:10 +0800

    Update App component
    - Convert it back from class to functional components
    - Our standard is a component should have its own directory

---
+++ src/App/index.js
@@ -0,0 +1,29 @@
+import logo from '../logo.svg';
+import './style.css';
+import Footer from '../Common/Footer';
+import Header from '../Common/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App/index.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
