c9bc6a831092f9f6a1e21d5b04d4309bbc9dfeab
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-12 14:55:12 +0800

    Added dummy pages

---
+++ src/Pages/About/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function About() {
+  return (
+    <div className="About">
+      <header className="About-header">About</header>
+      <p>This is my about page.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Pages/Portfolio/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+      <header className="Portfolio-header">Portfolio</header>
+      <p>This is my portfolio page.</p>
+    </div>
+  );
+}
+
+export default Portfolio;
