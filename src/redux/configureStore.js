6418c808fa3b76d6bea0dca038ab8d8015beaa06
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-30 22:53:54 +0800

    Renamed reducer file to index.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,8 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import aboutReducer from '../Pages/About/reducer/about';
-import portfolioReducer from '../Pages/Portfolio/reducer/portfolio';
+import aboutReducer from '../Pages/About/reducer';
+import portfolioReducer from '../Pages/Portfolio/reducer';
 import { watcherSaga } from './rootSaga';
 
 const reducers = combineReducers({
